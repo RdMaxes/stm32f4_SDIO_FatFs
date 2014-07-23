0b2f0e57fa421ff28bf48a21553c006150200600
RdMaxes <yc.huang0522@gmail.com>
2014-07-23 17:04:37 +0800

    read out data

--- Project/SDIO_FatFs/main.c
+++ Project/SDIO_FatFs/main.c
@@ -57,7 +57,7 @@ int main(void)
 				{
 					//clear buffer
 					for(stream_cnt=0;stream_cnt<READ_BUF_LEN;stream_cnt++)	rd_buf[stream_cnt] = 0x00;
-
+					res = f_read(&fsrc,rd_buf,READ_BUF_LEN,&br);
 				}
 			}
 		}
