404ef5d6cfcf72559594e00207bcd25ea3a0b8af
RdMaxes <yc.huang0522@gmail.com>
2014-07-23 18:30:55 +0800

    NA_test only

--- Project/SDIO_FatFs/main.c
+++ Project/SDIO_FatFs/main.c
@@ -18,8 +18,9 @@ UINT br, bw;		//file R/W counter
 DIR dir;			//directory 
 FILINFO FileInfo;	//file information 
 
-const char rootdir[] = "0:/"; //root directory
+const char rootdir[] = "/test"; //root directory of driver 0
 const char opnfile[] = "test.text"; //file name 
+const char driver0[] = "/\0";
 uint8_t rd_buf[READ_BUF_LEN] = {0};	//read out buffer
 
 //Delay for a while
@@ -41,41 +42,39 @@ int main(void)
 			LED_loop();
 		}
 	//print SD card size
-	my_printf("SD Card Size: %d Bytes\r\n",(uint32_t)SDCardInfo.CardCapacity);
+	my_printf("\r\nSD Card Size: %d Bytes\r\n",(uint32_t)SDCardInfo.CardCapacity);
 	//mount logical disk 0
-		f_mount(&fs,0,1);
-		res = f_opendir(&dir,rootdir); //open assigned directory
-		if(res==FR_OK)
+		res = f_mount(&fs,driver0,1);
+		if (res==FR_OK)
 		{
-			res = f_open(&fsrc,opnfile,FA_READ); //open assigned file
-			if(!res)
+			res = f_opendir(&dir,rootdir); //open assigned directory
+			if(res==FR_OK)
 			{
-				my_printf("Opening file: %s \r\n",opnfile);
-				my_printf("File Content:\r\n");
-				stream_cnt=0;
-				for(;;)
+				res = f_open(&fsrc,opnfile,FA_READ); //open assigned file
+				if(!res)
 				{
-					//clear buffer
-					for(stream_cnt=0;stream_cnt<READ_BUF_LEN;stream_cnt++)	rd_buf[stream_cnt] = 0x00;
-					//read out data with READ_BUF_LEN length
-					res = f_read(&fsrc,rd_buf,READ_BUF_LEN,&br); 
-					//print out
-					my_printf("%s",rd_buf);
-					//if error occur or reach end of file, break
-					if(res||(br<READ_BUF_LEN))	break;
+					my_printf("Opening file: %s \r\n",opnfile);
+					my_printf("File Content:\r\n");
+					stream_cnt=0;
+					for(;;)
+					{
+						//clear buffer
+						for(stream_cnt=0;stream_cnt<READ_BUF_LEN;stream_cnt++)	rd_buf[stream_cnt] = 0x00;
+						//read out data with READ_BUF_LEN length
+						res = f_read(&fsrc,rd_buf,READ_BUF_LEN,&br); 
+						//print out
+						my_printf("%s",rd_buf);
+						//if error occur or reach end of file, break
+						if(res||(br<READ_BUF_LEN))	break;
+					}
 				}
+				else	my_printf("Fatal, no such file.\r\n");
 			}
-			else
-			{
-				my_printf("Fatal, no such file.\r\n");
-			}
+			else	my_printf("Fatal, no such directory.\r\n");
 		}
-		else
-		{
-			my_printf("Fatal, no such directory.\r\n");
-		}
+		else	my_printf("Fatal, driver mounting error.\r\n");
 
-		f_mount(0x00,0,1);	
+		f_mount(0x00,driver0,1);	
 	//unmount logical disk 0
 	while(1) 
 	{
